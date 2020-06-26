20ba666d22a989926635a111ab2f97f47d743597
monikaklim <monika.klim@scaiconsulting.it>
2020-06-26 15:37:37 +0200

    terminated delete user

--- scaiVisit/src/app/users/users.service.ts
+++ scaiVisit/src/app/users/users.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import {environment} from './../../environments/environment';
 import { Subject } from 'rxjs';
 import { User } from './user.model';
+import { LoadingController } from '@ionic/angular';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ import { User } from './user.model';
 export class UsersService {
 private apiUrl:string = environment.apiUrl;
 
-constructor(public http: HttpClient) { }
+constructor(public http: HttpClient, public loadingController:LoadingController) { }
 
 private users:User[] = [];
 usersChanged = new Subject<User[]>();
@@ -35,12 +36,16 @@ return this.http.get(this.apiUrl + "usersenabled").subscribe(res => this.setUser
 }
 
 deleteUser(userId:string){
-
-  const index = this.users.findIndex(u => u.userId === userId);
-  this.users.splice(index,1);
- this.http.delete(this.apiUrl + "user/" + userId);
+  this.loadingController.create({
+    message: "Eliminazione in corso..."
+  }).then(loadingEl => {
+    loadingEl.present();
+const index = this.users.findIndex(u => u.userId === userId);
+ this.users.splice(index,1);
+ this.http.delete(this.apiUrl + "user/" + userId).subscribe(res => {console.log(res); loadingEl.dismiss()});
  this.usersChanged.next(this.users)
  console.log("delete service")
+  });
 }
 
 
