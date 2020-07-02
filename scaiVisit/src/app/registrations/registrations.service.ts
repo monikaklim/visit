75d42c564f68a7af8ccae81ea39b77411a8de86a
monikaklim <monika.klim@scaiconsulting.it>
2020-07-02 12:48:59 +0200

    created registrations service

--- scaiVisit/src/app/registrations/registrations.service.ts
+++ scaiVisit/src/app/registrations/registrations.service.ts
@@ -4,6 +4,8 @@ import { Subject } from 'rxjs';
 import { Company } from '../companies/company.model';
 import { HttpClient } from '@angular/common/http';
 import { LoadingController } from '@ionic/angular';
+import { Registration } from './registration.model';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +13,54 @@ import { LoadingController } from '@ionic/angular';
 export class RegistrationsService {
 
 private apiUrl:string = environment.apiUrl;
-
-companiesChanged = new Subject<Company[]>();
+private registrations: Registration[];
+registrationsChanged = new Subject<Registration[]>();
 
 
 constructor(public http: HttpClient, public loadingController:LoadingController) { }
 
 
+  setRegistrations(registrations:Registration[]){
+    this.registrations = registrations;
+  }
+
+  getRegistrations(){
+    return this.registrations;
+  }
+
+  findRegistrazioniToday(sede) {
+
+    return this.http.get<Registration[]>(this.apiUrl + "registrationstoday/" + sede).subscribe(res => this.setRegistrations(res));
+  }
+
+  findRegistrazioni(filtro) {
+
+    return this.http.post<Registration[]>(this.apiUrl + "registrationsfiltered", filtro).subscribe(res => this.setRegistrations(res) );
+  }
+
+  findRegistrazioniPdf(filtro) {
+
+    return this.http.post<Registration[]>(this.apiUrl + "registrationsfilteredpdf", filtro).subscribe(res => console.log(res));
+  }
+
+  saveRegistration(visita) {
+    this.registrations.push(visita);
+    return this.http.post(this.apiUrl + "registration", visita).subscribe(res => console.log(res));
+  }
+
+  closeRegistration(idVisita) {
+
+    return this.http.post(this.apiUrl + "closeregistration", idVisita).subscribe(res => console.log(res));
+  }
+
+  countVisitors(filtro) {
+
+    return this.http.post(this.apiUrl + "countdistinctperday", filtro).subscribe(res => console.log(res));
+  }
+
+  countVisitorsEnabled(filtro) {
+
+    return this.http.post(this.apiUrl + "countdistinctperdayenabled", filtro).subscribe(res => console.log(res));
+  }
 
 }
